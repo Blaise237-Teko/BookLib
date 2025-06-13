4ae983cf6ec8165ca5bbba7b1c4544b5ff48fe50
Teko Blaise <tekoblaise20055@gmail.com>
2025-06-13 15:25:08 +0100

    Initial project upload

---
+++ src/model/Book.java
@@ -0,0 +1,10 @@
+package model;
+
+public class Book {
+    private String title;
+    private String author;
+    private String genre;
+    private boolean isRead;
+
+    // Constructors, getters, setters here
+}
---
+++ src/util/Database.java
@@ -0,0 +1,5 @@
+package util;
+
+public class Database {
+    // JDBC SQLite connection code goes here
+}
